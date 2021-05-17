7b0f1d3a688e19bd72767dabe3dae4cf2faade7a
VD42 <vladislav.kochemaev@gmail.com>
2021-05-17 12:20:33 +0400

    Crash check for tests

--- tests/_run.bat
+++ tests/_run.bat
@@ -1,1 +1,6 @@
 ..\x64\Release\UE4TextExtractor.exe . _texts.txt
+if errorlevel 1 (
+    echo. >> _texts.txt
+    echo. >> _texts.txt
+    echo !!!!!!!!!!!!!!!!!!!!!!!!CRASH!!!!!!!!!!!!!!!!!!!!!!!! >> _texts.txt
+)
