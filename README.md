768bc036c160cb456e4b96cc5fd3427ac5e5ccc5
VD42 <vladislav.kochemaev@gmail.com>
2024-07-29 13:00:54 +0400

    New readme info

--- README.md
+++ README.md
@@ -6,11 +6,12 @@ Extract localizable text from .uasset, .uexp and .umap files and convert locres 
 
 Extract localizable texts to locres or txt file:  
 `UE4TextExtractor.exe <path to folder with extracted from pak files> <path to texts.locres file> [-old] [-raw-text-signatures=<signature1>,<signature2>,...] [-all-uexps]`  
-`UE4TextExtractor.exe <path to folder with extracted from pak files> <path to texts.txt file> [-raw-text-signatures=<signature1>,<signature2>,...] [-all-uexps]`  
+`UE4TextExtractor.exe <path to folder with extracted from pak files> <path to texts.txt file> [-raw-text-signatures=<signature1>,<signature2>,...] [-all-uexps] [-src]`  
 Example: `UE4TextExtractor.exe "C:\MyGame\Content\Paks\unpacked" "C:\MyGame\Content\Paks\texts.locres"`  
   
 Use `-raw-text-signatures=<signature1>,<signature2>,...` (or `-raw-text-signatures=all` if you don't want to go into detail, but it's not recommended) modifier for parsing localizable text by custom signatures. See also: [here](https://github.com/VD42/UE4TextExtractor/blob/master/RAW_TEXT_SIGNATURES.md).  
 Use `-all-uexps` modifier for additionaly parsing uexp files without matching uasset or umap files.  
+Use `-src` modifier to add string source information (filenames) to the txt file.  
   
 Convert locres to txt or backward:  
 `UE4TextExtractor.exe <path to texts.txt file> <path to texts.locres file> [-old]`  
--- main.cpp
+++ main.cpp
@@ -686,12 +686,13 @@ void print_help()
 	std::wcout
 		<< L"Extract localizable texts to locres or txt file:" << std::endl
 		<< L"UE4TextExtractor.exe <path to folder with extracted from pak files> <path to texts.locres file> [-old] [-raw-text-signatures=<signature1>,<signature2>,...] [-all-uexps]" << std::endl
-		<< L"UE4TextExtractor.exe <path to folder with extracted from pak files> <path to texts.txt file> [-raw-text-signatures=<signature1>,<signature2>,...] [-all-uexps]" << std::endl
+		<< L"UE4TextExtractor.exe <path to folder with extracted from pak files> <path to texts.txt file> [-raw-text-signatures=<signature1>,<signature2>,...] [-all-uexps] [-src]" << std::endl
 		<< LR"(Example: UE4TextExtractor.exe "C:\MyGame\Content\Paks\unpacked" "C:\MyGame\Content\Paks\texts.locres")" << std::endl
 		<< std::endl
 
 		<< L"Use -raw-text-signatures=<signature1>,<signature2>,... (or -raw-text-signatures=all if you don't want to go into detail, but it's not recommended) modifier for parsing localizable text by custom signatures. See also: https://github.com/VD42/UE4TextExtractor/blob/master/RAW_TEXT_SIGNATURES.md." << std::endl
 		<< L"Use -all-uexps modifier for additionaly parsing uexp files without matching uasset or umap files." << std::endl
+		<< L"Use -src modifier to add string source information (filenames) to the txt file." << std::endl
 		<< std::endl
 
 		<< L"Convert locres to txt or backward:" << std::endl
